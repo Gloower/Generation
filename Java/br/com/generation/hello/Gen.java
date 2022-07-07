74de25565ef8da82d43f165fe41ed5fc5d019a3b
Gloower <gloowerzin@gmail.com>
2022-07-07 11:06:27 -0300

    Learning java

---
+++ Java/br/com/generation/hello/Gen.java
@@ -0,0 +1,21 @@
+<<<<<<< HEAD:Java/br/com/generation/hello/Gen.java
+package br.com.generation.hello;
+
+public class Gen {
+    public static void main(String args[]) {
+        System.out.println("Ola");
+        
+    } 
+
+}
+=======
+package br.com.generation.hello;
+
+public class Gen {
+    public static void main(String args[]) {
+        System.out.println("Ola");
+        
+    } 
+
+}
+>>>>>>> 82cd53b58b498fea3edd12115ce946e3114c2a00:Gen.java
