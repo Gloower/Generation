8711549a4449636ea10c93a9d3288d02b1dc96ee
Gloower <gloowerzin@gmail.com>
2022-08-02 17:42:54 -0300

    Tabela sql

--- Spring/blogpessoal56/tabela_blog.sql
+++ Spring/blogpessoal56/tabela_blog.sql
@@ -1,10 +1,6 @@
-USE db_blogspring;
-INSERT INTO tb_temas (data, temas_descrição, temas_nome)
-VALUES (current_timestamp(), 'Olá hoje eu estou com a minha namorada incrivel', 'Nós');
-INSERT INTO tb_temas (data, temas_descrição, temas_nome)
-VALUES (current_timestamp(), 'Hoje vou comer chocolate com a minha namorada incrivel', 'A gente');
-INSERT INTO tb_temas(data, temas_descrição, temas_nome)
-VALUES(CURRENT_TIMESTAMP(), 'Amo ela! <3', 'Ela');
-
-
-SELECT * FROM tb_temas;
+USE db_farmaciatop;
+INSERT INTO tb_produtos (data, produto_nome, produto_descricao)
+VALUES (current_timestamp(), 'Dipirona', 'Usado para dor de cabeça');
+INSERT INTO tb_produtos (data, produto_nome, produto_descricao)
+VALUES (current_timestamp(), 'Ibuprofeno', 'Usado para alguma coisa');
+SELECT * FROM tb_produtos;
