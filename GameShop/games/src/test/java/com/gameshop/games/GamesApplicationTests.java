fceadd68484e5a0321b0f68df1b9950954d1afd0
Gloower <gloowerzin@gmail.com>
2022-08-05 13:57:19 -0300

    Loja de games <3

---
+++ GameShop/games/src/test/java/com/gameshop/games/GamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gameshop.games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
